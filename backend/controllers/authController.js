e22015a637a982b24b7be37506de91af4e666adf
Deepshika Singh <deepshikasingh110@gmail.com>
2026-05-28 22:23:21 +0530

    fixed production auth cookies

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -72,11 +72,11 @@ export const login = async (req ,res)=>{
   const token = generateToken(user);
   user.password = undefined;
   res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "strict",
-      maxAge: 24 * 60 * 60 * 1000,
-    });
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 24 * 60 * 60 * 1000,
+});
     console.log(res.data);
   return res.status(200).json({
     success:true,
@@ -96,9 +96,11 @@ export const login = async (req ,res)=>{
 
 export const logout = async (req, res) => {
   res.cookie("token", "", {
-    httpOnly: true,
-    expires: new Date(0),
-  });
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  expires: new Date(0),
+});
 
   return res.status(200).json({
     success: true,
